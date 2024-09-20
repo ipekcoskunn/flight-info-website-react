eccfd0f53a867646dde71e65f56337a25c91ea3c
ipekcoskun <ipekcoskun@omegabigdata.com>
2024-09-20 21:33:39 +0300

    homepage

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import RightCards from './RightCards'
 import FilterFlightCard from './FilterFlightCard'
+import FlightCard from './FlightCard'
 
 const Home = () => {
     return (
         <div className="home-section">
             <div className="container">
-                <div className='row'>
+                <div className='row homepage'>
                     <div className='col-lg-9 col-md-9 col-xs-12 col-sm-12 my-3'>
                         <FilterFlightCard />
                         <div className="row">
                             <div className="col-lg-9 col-md-10 col-xs-12 col-sm-12 my-2">
-                                uçuş bilgileri
+                                <FlightCard />
                             </div>
                             <div className="col-lg-3 col-md-2 col-xs-12 col-sm-12 my-2">
                                 sort by
